25441beb738f84bf247983054215e4f4c53c01e3
Nick <info@cloudelements.ca>
2023-05-03 00:38:59 -0400

    adding provider

--- providers.tf
+++ providers.tf
@@ -2,3 +2,12 @@
 ## ALL PROVIDERS REQUIRED FOR MODULE
 ## Add all required providers below.
 ## ---------------------------------------------------------------------------------------------------------------------
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.54.0"
+    }
+  }
+}
