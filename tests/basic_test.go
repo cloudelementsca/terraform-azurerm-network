4a2f781175082fd45d0b49073dd32ce9b5d6a427
nicksnb <info@cloudelements.ca>
2023-05-09 18:50:43 -0400

    basic_test cleanup

--- tests/basic_test.go
+++ tests/basic_test.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
-	//"github.com/stretchr/testify/require"
 )
 
 func TestBasicNetworkModule(t *testing.T) {
@@ -17,11 +16,8 @@ func TestBasicNetworkModule(t *testing.T) {
 		Dns_servers  			[]string
 		Edge_zone				string
 		Flow_timeout_in_minutes int
-		//Guid					string
-		//Id						string
 		Location				string
 		Resource_group_name		string
-		//Timeouts				[]string
 		Tags					map[string]interface{}
 	}
 
@@ -32,32 +28,11 @@ func TestBasicNetworkModule(t *testing.T) {
 		Dns_servers: 			 []string{},
 		Edge_zone:				 "",
 		Flow_timeout_in_minutes: 0,
-		//Guid:					 "",
-		//Id:						 "",
 		Location:				 "canadacentral",
 		Resource_group_name:	 "vnet-rg",
-		//Timeouts:				 nil,
 		Tags:					 map[string]interface{}{"environment":"dev"},
 	}
 
-//	expectedVnetOutput := `{
-//		"address_space":["10.0.0.0/8"],
-//		"bgp_community":"",
-//		"ddos_protection_plan":[],
-//		"dns_servers":[],
-//		"edge_zone":"",
-//		"flow_timeout_in_minutes":0,
-//		"guid":"549bfbaf-a8b7-4e5d-8419-8a400c1961d2",
-//		"id":"/subscriptions/***/resourceGroups/vnet-rg/providers/Microsoft.Network/virtualNetworks/vnet-qzonuvo9",
-//		"location":"canadacentral",
-//		"name":"vnet-qzonuvo9",
-//		"resource_group_name":"vnet-rg",
-//		"subnet":[],
-//		"tags":{"environment":"dev"},
-//		"timeouts":null
-//	}`
-	
-	// retryable errors in terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "../examples/basic",
 	})
@@ -67,9 +42,10 @@ func TestBasicNetworkModule(t *testing.T) {
 	terraform.InitAndApply(t, terraformOptions)
 
 	actualObject := VnetStruct{}
-	//terraform.OutputStruct(t, terraformOptions, "vnet", &actualObject)
+
 	str := terraform.OutputJson(t, terraformOptions, "vnet")
+
 	json.Unmarshal([]byte(str), &actualObject)
+
 	assert.Equal(t, expectedVnetOutput, actualObject, &actualObject)
-	//require.Equal(t, str, expectedVnetOutput, "JSON %q should match %q", expectedVnetOutput, str)
 }
