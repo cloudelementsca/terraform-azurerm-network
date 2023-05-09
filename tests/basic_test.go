b0246eca7bcc07dd10a13f680aea9bcac691ee68
nicksnb <info@cloudelements.ca>
2023-05-08 23:16:51 -0400

    adjusting for json

--- tests/basic_test.go
+++ tests/basic_test.go
@@ -5,52 +5,53 @@ import (
 	"encoding/json"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestBasicNetworkModule(t *testing.T) {
 
-	type VnetStruct struct {
-		address_space    		[]string
-		bgp_community   		string
-		ddos_protection_plan    []string
-		dns_servers  			[]string
-		edge_zone				string
-		flow_timeout_in_minutes int
-		location				string
-		resource_group_name		string
-		timeouts				[]string
-		tags					map[string]interface{}
-	}
+//	type VnetStruct struct {
+//		address_space    		[]string
+//		bgp_community   		string
+//		ddos_protection_plan    []string
+//		dns_servers  			[]string
+//		edge_zone				string
+//		flow_timeout_in_minutes int
+//		location				string
+//		resource_group_name		string
+//		timeouts				[]string
+//		tags					map[string]interface{}
+//	}
+//
+//	expectedVnetOutput := VnetStruct{
+//		address_space:    		 []string{"10.0.0.0/8"},
+//		bgp_community:   		 "",
+//		ddos_protection_plan:    []string{},
+//		dns_servers: 			 []string{},
+//		edge_zone:				 "",
+//		flow_timeout_in_minutes: 0,
+//		location:				 "canadacentral",
+//		resource_group_name:	 "vnet-rg",
+//		timeouts:				 nil,
+//		tags:					 map[string]interface{}{"environments":"dev"},
+//	}
 
-	expectedVnetOutput := VnetStruct{
-		address_space:    		 []string{"10.0.0.0/8"},
-		bgp_community:   		 "",
-		ddos_protection_plan:    []string{},
-		dns_servers: 			 []string{},
-		edge_zone:				 "",
-		flow_timeout_in_minutes: 0,
-		location:				 "canadacentral",
-		resource_group_name:	 "vnet-rg",
-		timeouts:				 nil,
-		tags:					 map[string]interface{}{"environments":"dev"},
-	}
-
-//	expectedVnetOutput := `{
-//		"address_space":["10.0.0.0/8"],
-//		"bgp_community":"",
-//		"ddos_protection_plan":[],
-//		"dns_servers":[],
-//		"edge_zone":"",
-//		"flow_timeout_in_minutes":0,
-//		"guid":"549bfbaf-a8b7-4e5d-8419-8a400c1961d2",
-//		"id":"/subscriptions/***/resourceGroups/vnet-rg/providers/Microsoft.Network/virtualNetworks/vnet-qzonuvo9",
-//		"location":"canadacentral",
-//		"name":"vnet-qzonuvo9",
-//		"resource_group_name":"vnet-rg",
-//		"subnet":[],
-//		"tags":{"environment":"dev"},
-//		"timeouts":null
-//	}`
+	expectedVnetOutput := `{
+		"address_space":["10.0.0.0/8"],
+		"bgp_community":"",
+		"ddos_protection_plan":[],
+		"dns_servers":[],
+		"edge_zone":"",
+		"flow_timeout_in_minutes":0,
+		"guid":"549bfbaf-a8b7-4e5d-8419-8a400c1961d2",
+		"id":"/subscriptions/***/resourceGroups/vnet-rg/providers/Microsoft.Network/virtualNetworks/vnet-qzonuvo9",
+		"location":"canadacentral",
+		"name":"vnet-qzonuvo9",
+		"resource_group_name":"vnet-rg",
+		"subnet":[],
+		"tags":{"environment":"dev"},
+		"timeouts":null
+	}`
 	
 	// retryable errors in terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
@@ -61,7 +62,9 @@ func TestBasicNetworkModule(t *testing.T) {
 
 	terraform.InitAndApply(t, terraformOptions)
 
-	actualObject := VnetStruct{}
-	terraform.OutputStruct(t, terraformOptions, "vnet", &actualObject)
-	assert.Equal(t, expectedVnetOutput, json.Unmarshal(actualObject, &actualObject))
+	//actualObject := VnetStruct{}
+	//terraform.OutputStruct(t, terraformOptions, "vnet", &actualObject)
+	str := terraform.OutputJson(t, terraformOptions, "")
+	//assert.Equal(t, expectedVnetOutput, actualObject, &actualObject)
+	require.Equal(t, str, expectedVnetOutput, "JSON %q should match %q", expectedVnetOutput, str)
 }
