3b9cae300fd29cfcfe0b877ea9d8c09e3f60cec8
nicksnb <info@cloudelements.ca>
2023-05-20 16:00:05 -0400

    adjusting syntax

--- tests/multi_subnet_test.go
+++ tests/multi_subnet_test.go
@@ -36,7 +36,7 @@ type SubnetStruct struct {
 	Private_link_service_network_policies_enabled bool
 }
 
-func TestBasicNetworkModule(t *testing.T) {
+func TestMultiSubnetNetworkModule(t *testing.T) {
 
 	expectedVnetOutput := VnetStruct{
 		Address_space:           []string{"10.19.0.0/16"},
@@ -60,9 +60,13 @@ func TestBasicNetworkModule(t *testing.T) {
 	expectedAciSubnetOutupt := SubnetStruct{
 		Address_prefixes: []string{"10.19.2.0/24"},
 		Delegation: []DelegationStruct{
-			Service_delegation: []ServiceDelegationStruct{
-				Name:    "Microsoft.ContainerInstance/containerGroups",
-				Actions: []string{"Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"},
+			Aci_Delegation: {
+				Service_delegation: []ServiceDelegationStruct{
+					Aci_Service_Delegation: {
+						Name:    "Microsoft.ContainerInstance/containerGroups",
+						Actions: []string{"Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"},
+					},
+				},
 			},
 		},
 		Private_endpoint_network_policies_enabled:     true,
@@ -85,10 +89,8 @@ func TestBasicNetworkModule(t *testing.T) {
 	terraform.InitAndApply(t, terraformOptions)
 
 	actualObject := VnetStruct{}
-
 	str := terraform.OutputJson(t, terraformOptions, "vnet")
-
 	json.Unmarshal([]byte(str), &actualObject)
-
 	assert.Equal(t, expectedVnetOutput, actualObject, &actualObject)
+
 }
